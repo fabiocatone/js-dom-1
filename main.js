e51ac790df166008cfeec8f118aa15f3b828192b
fabiocatone <fabbiocatone@gmail.com>
2026-03-17 18:19:40 +0100

    finish bonus

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const imgEl = document.querySelector("img")
+const buttEl = document.querySelector ("button")
 console.log(imgEl);
-imgEl.addEventListener("click", function (){
-    console.log("clicked");
-    
-    imgEl.src = "./img/yellow_lamp.png"
-})
+console.log(buttEl);
+
+
+
+buttEl.addEventListener("click", function () {
+     console.log("clicked");
+    if (buttEl.innerHTML.includes("Accendi")) {
+       imgEl.src = "./img/yellow_lamp.png";
+    buttEl.innerHTML = "Spegni"} 
+       else if (buttEl.innerHTML = "Spegni") {
+        imgEl.src = "./img/white_lamp.png";
+        buttEl.innerHTML = "Accendi"
+       }
+    }
+     )
